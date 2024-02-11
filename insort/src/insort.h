4e79910d09aae0688f00a1199e92e7b7111c66c6
outragedline <outragedline@proton.me>
2024-02-11 14:36:03 -0300

    Insort

---
+++ insort/src/insort.h
@@ -0,0 +1,1 @@
+int *insort(int[], int);
---
+++ insort/src/main.c
@@ -0,0 +1,12 @@
+#include "insort.h"
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	int arr[] = { 5, 2, 4, 6, 3 };
+	int size = (int)sizeof(arr) / sizeof(arr[0]);
+	int *sorted = insort(arr, size);
+
+	for (int i; i < size; i++) {
+		printf("%d\n", sorted[i]);
+	}
+}
